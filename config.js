e9d04b9861c2f504ebc1338a8a591d5999ab8d37
rajveersinghyadav <storynovausaofficial@gmail.com>
2026-07-16 14:54:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 // config.js
 module.exports = {
-    SYMBOL: 'BTCUSD',
+    SYMBOL: ['XAUUSD', 'BTCUSD', 'EURUSD', 'GBPUSD'],
     QUANTITY: 0.1,
     PROFIT_TARGET_USD: 10.0, 
     TIMEFRAME: '1m',
 };
+
